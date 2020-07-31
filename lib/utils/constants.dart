eae3b331ed42aa9117a00d1a4300ba596d972590
Giancarlo Haack Albano <gian418@gmail.com>
2020-07-30 22:12:54 -0300

    remove key

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -4,5 +4,5 @@ class Constants {
       'https://identitytoolkit.googleapis.com/v1/accounts:';
   static const AUTH_SINGUP_URL = 'signUp?key=';
   static const AUTH_LOGIN_URL = 'signInWithPassword?key=';
-  static const API_KEY = 'AIzaSyDXQBVizDpwNrYo7a79s3hRAChqf-Pr5PM';
+  static const API_KEY = '';
 }
